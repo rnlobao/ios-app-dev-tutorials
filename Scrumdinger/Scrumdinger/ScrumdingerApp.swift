159b0400f1c3ce9c87e0fa521decb7fe6b2c7c2e
Robson Novato <66230142+rnlobao@users.noreply.github.com>
2021-12-14 00:21:18 -0300

    Understanding SwiftUI

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Robson Novato Lobao on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Meeting_View()
+        }
+    }
+}
