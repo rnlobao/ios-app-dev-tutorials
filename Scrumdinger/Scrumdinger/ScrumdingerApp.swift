ac8ce4d0822dd772fcde1970b99177039484e62f
Robson Novato <66230142+rnlobao@users.noreply.github.com>
2022-02-11 15:39:45 -0300

    Views

--- Scrumdinger/Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -2,7 +2,7 @@
 //  ScrumdingerApp.swift
 //  Scrumdinger
 //
-//  Created by Robson Novato Lobao on 13/12/21.
+//  Created by Robson Novato Lobao on 11/02/22.
 //
 
 import SwiftUI
@@ -11,10 +11,7 @@ import SwiftUI
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                ScrumsView(scrums: DailyScrum.sampleData)
-            }
+            MeetingView()
         }
     }
 }
-
